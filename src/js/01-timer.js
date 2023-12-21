98f4bf84fea0ed527c64952e9b3d22820adf4ad8
serhiyserhiychuk <141840178+serhiyserhiychuk@users.noreply.github.com>
2023-12-20 22:42:09 -0500

    Doing Homework

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -3,10 +3,10 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 const timeInput = document.querySelector('input#datetime-picker');
 const startButton = document.querySelector('button[data-start]');
-const daySpan = document.querySelector('spam[data-days]');
-const hourSpan = document.querySelector('spam[data-hours]');
-const minuteSpan = document.querySelector('spam[data-minutes]');
-const secondSpan = document.querySelector('spam[data-seconds]');
+const daySpan = document.querySelector('span[data-days]');
+const hourSpan = document.querySelector('span[data-hours]');
+const minuteSpan = document.querySelector('span[data-minutes]');
+const secondSpan = document.querySelector('span[data-seconds]');
 
 const options = {
   enableTime: true,
@@ -14,25 +14,46 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    dateCheck(selectedDates);
+    let userSelectedDate = selectedDates[0].getTime();
+    let currentDate = new Date().getTime();
+    if (currentDate > userSelectedDate) {
+      window.alert('Please choose a date in the future');
+      startButton.disabled = true;
+    } else if (currentDate < userSelectedDate) {
+      startButton.disabled = false;
+    }
   },
 };
 
 flatpickr(timeInput, options);
 
-function dateCheck(selectedDates) {
-  let userSelectedDate = selectedDates[0].getTime();
-  let currentDate = new Date().getTime();
-  if (currentDate > userSelectedDate) {
-    window.alert('Please choose a date in the future');
-    startButton.disabled = true;
-  } else if (currentDate < userSelectedDate) {
-    startButton.disabled = false;
-  }
-}
-
 startButton.addEventListener('click', timerHandler);
 
 function timerHandler(event) {
   event.preventDefault();
+  let ms = options.userSelectedDate - new Date().getTime();
+  console.log(options.userSelectedDate);
+  daySpan.textContent = convertMs(ms).days;
+  hourSpan.textContent = convertMs(ms).hours;
+  minuteSpan.textContent = convertMs(ms).minutes;
+  secondSpan.textContent = convertMs(ms).seconds;
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
 }
